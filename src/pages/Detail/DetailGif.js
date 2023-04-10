77859c319f402454e46c4a1b9717a01ad74ab910
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2023-04-09 22:31:19 -0300

    se agrego una función Math Random para simular un numero de descargas

--- src/pages/Detail/DetailGif.js
+++ src/pages/Detail/DetailGif.js
@@ -10,10 +10,23 @@ const Detail = ({params}) => {
     const gifUnico = gif.find(singleGif => singleGif.id === params.id)
     console.log({gifUnico})
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+      
+      console.log();
+      // Expected output: 0, 1 or 2
     return (
         <>
         <h2>Gif Seleccionado</h2>
         <Gif {...gifUnico}/>
+        <div>
+            <h3>Info del Gif</h3>
+            <h3>Nombre: <h5>{gifUnico.title}</h5></h3>
+            <h3>ID: {gifUnico.id}</h3>
+            <h3>URL: {gifUnico.url}</h3>
+            <h3>Cantidad de descargas: {getRandomInt(100)}</h3>
+        </div>
         </>
     )
 }
